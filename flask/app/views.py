ee950ec2111212f3daddba33732ba47bf85d2ee1
Shevtsov Sergey <dev.serzh@gmail.com>
2020-03-29 11:57:32 +0300

    error handling

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,14 +1,20 @@
 from flask.views import MethodView
 from flask import request, make_response
-from app import app
+from app import app, db
 
 from app.models import Rating
 from app.serializers import RatingSchema
 
 
-@app.route('/')
-def index():
-    return '<h1>Flask</h1>'
+@app.errorhandler(404)
+def not_found_error(error):
+    return make_response({'msg': 'no such route'}, 404)
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return make_response({'msg': 'something went wrong'}, 500)
 
 
 class RatingView(MethodView):
@@ -20,6 +26,8 @@ class RatingView(MethodView):
         if not imdb_id:
             return make_response({'error': 'imdb_id is required'}, 400)
         rating = Rating.query.filter_by(imdb_id=imdb_id).first()
+        if not rating:
+            return make_response({'msg': 'such imdb_id not found'}, 404)
         return self.rating_schema.dump(rating)
 
 
